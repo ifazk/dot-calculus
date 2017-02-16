1c79a30e2d545d2f6932e474c4fc95e2df726e03
Ifaz Kabir <ifazk@users.noreply.github.com>
2017-02-16 02:32:38 -0500

    Removing comments from safety proof file.

--- dev/lf/good-proof/Dot_proofs_safety.v
+++ dev/lf/good-proof/Dot_proofs_safety.v
@@ -5,19 +5,8 @@ Require Import Coq.Program.Equality.
 Require Import Dot_definitions.
 Require Import Dot_proofs_weakening.
 Require Import Dot_proofs_wellformed_store.
-(*
-Require Import Dot_proofs_extra_rec.
- *)
 Require Import Dot_proofs_substitution.
-(*
-Require Import Dot_proofs_some_lemmas.
- *)
 Require Import Dot_proofs_narrowing.
-(*
-Require Import Dot_proofs_has_member.
-Require Import Dot_proofs_tight_bound_completeness.
-Require Import Dot_proofs_misc_inversions.
-*)
 Require Import Dot_proofs_possible_types.
 Require Import Dot_proofs_misc.
 
