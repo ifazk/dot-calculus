59c04285566f8b3168c8d0a59bbc1aa35391d2b6
Paul He <paul.he@edu.uwaterloo.ca>
2017-03-15 21:50:56 -0400

    Added the updated wf_good from the good directory to tight

--- dev/lf/tight-proof/Dot_proofs_good_types.v
+++ dev/lf/tight-proof/Dot_proofs_good_types.v
@@ -40,6 +40,20 @@ Inductive good : ctx -> Prop :=
       good_typ T ->
       good (pre & x ~ T).
 
+Lemma wf_good : forall G s, wf_sto G s -> good G.
+Proof.
+  intros. induction H.
+  - apply good_empty.
+  - apply good_all.
+    + assumption.
+    + dependent induction H2.
+      * apply good_typ_all.
+      * apply good_typ_bnd. pick_fresh z. apply open_record_type_rev with (x:=z); auto.
+        apply record_defs_typing with (G:=G & z ~ open_typ z T) (ds:= open_defs z ds); auto. 
+      * assert (ty_precise = ty_precise) by reflexivity. apply H4 in H5.
+        destruct H5. inversion H5.
+Qed.
+
 (* Good contexts bind good:
 If G |- x : T and G is a good context then T is a good type. *)
 
