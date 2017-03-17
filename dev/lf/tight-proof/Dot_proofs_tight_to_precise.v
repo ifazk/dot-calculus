ae1b54a799a9717b702eaf13a335a4ee827c9d65
Paul He <paul.he@edu.uwaterloo.ca>
2017-03-17 12:20:19 -0400

    Added lemma 2, theorem 1

--- dev/lf/tight-proof/Dot_proofs_tight_to_precise.v
+++ dev/lf/tight-proof/Dot_proofs_tight_to_precise.v
@@ -27,3 +27,37 @@ Proof.
   - specialize (IHHtp Hwf HG). destruct IHHtp as [V [Hx [Hs1 Hs2]]].
     exists V. split*.
 Qed.
+
+(* Lemma 2 *)
+Lemma tight_subtyping_sel: forall G s x A S U,
+    wf_sto G s ->
+    ty_trm ty_general sub_tight G (trm_var (avar_f x)) (typ_rcd (dec_typ A S U)) ->
+    (subtyp ty_general sub_tight G (typ_sel (avar_f x) A) U /\
+     subtyp ty_general sub_tight G S (typ_sel (avar_f x) A)).
+Proof.
+  introv Hwf Hty. 
+  lets H: (tight_to_precise_typ_dec Hwf Hty). destruct H as [T [Ht [Hs1 Hs2]]].
+  split.
+  - apply subtyp_sel1_tight in Ht. apply subtyp_trans with (T:=T); auto.
+  - apply subtyp_sel2_tight in Ht. apply subtyp_trans with (T:=T); auto.
+Qed.
+
+(* Theorem 1 *)
+Lemma general_to_tight: forall G0 s0,
+  wf_sto G0 s0 ->
+  (forall m1 m2 G t T,
+     ty_trm m1 m2 G t T ->
+     G = G0 ->
+     m1 = ty_general ->
+     m2 = sub_general ->
+     ty_trm ty_general sub_tight G t T) /\
+  (forall m1 m2 G S U,
+     subtyp m1 m2 G S U ->
+     G = G0 ->
+     m1 = ty_general ->
+     m2 = sub_general ->
+     subtyp ty_general sub_tight G S U).
+Proof.
+  intros G0 s0 Hwf.
+  apply ts_mutind; intros; subst; eauto; apply* tight_subtyping_sel.
+Qed.
