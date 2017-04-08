765e15bc6752bc36acf06aa7c399a9d891bacd7e
Marianna Rapoport <mrapoport@uwaterloo.ca>
2017-04-08 19:07:56 -0400

    canonical forms 1 finally done

--- dev/lf/tight-proof/Canonical_forms1.v
+++ dev/lf/tight-proof/Canonical_forms1.v
@@ -30,30 +30,30 @@ Proof.
   pose proof (corresponding_types Hwf Hgd Bi)
     as [[L [S [V [S1 [V1 [t [Hb [Ht [Heq [Hs1 Hs2]]]]]]]]]] | [S [ds [Hb [Ht Heq]]]]].
   subst. inversion Heq; subst. inversions Ht.
-  - exists (L \u L' \u L0) S t.
+  - exists (L \u L' \u L0 \u dom G) S t.
     split; auto.
     pose proof (tight_possible_types_lemma Hgd Hti) as Htp.
+    assert (forall y W, y # G -> ok (G & y ~ W)) as Hok by (intros; apply* ok_push).
     inversion Htp; subst.
     + apply (good_precise_all_inv Hgd) in Hpt.
       apply (good_precise_all_inv Hgd) in H.
       pose proof (binds_func Hpt H) as H4.
       inversion H4; subst T U; clear H4.
       split. auto. intros y Hy.
-      apply narrow_typing with (G:=G & y ~ S).
+      assert (y \notin L0) as Hy0 by auto. assert (y \notin L) as HyL by auto.
+      specialize (H3 y Hy0). apply narrow_typing with (G':=G & y ~ S1) in H3.
       apply ty_sub with (T:=open_typ y V). intro Contra. inversion Contra.
-      apply* H3.
-      apply* H4.
+      assumption. apply* Hs2. apply* subenv_last. apply* Hok.
     + apply tight_to_general in HSsub; auto.
-      split; auto.
-      subst.
-      intros y Hfr.
-      eapply ty_sub.
-      intros Contra; inversion Contra.
-      eapply narrow_typing.
-      eapply H3; eauto.
-      apply subenv_last; auto.
-      apply ok_push. eapply wf_sto_to_ok_G; eauto. auto.
-      apply ok_push. eapply wf_sto_to_ok_G; eauto. auto.
-      auto.
+      assert (subtyp ty_general sub_general G T S) as HTS by (apply* subtyp_trans).
+      split; auto. intros y Hy. assert (y \notin L0) as Hy0 by auto.
+      specialize (H3 y Hy0).
+      apply narrow_typing with (G':=G & y ~ T) in H3. apply ty_sub with (T:=open_typ y V).
+      intro Contra. inversion Contra. assumption. apply subtyp_trans with (T:=open_typ y V1).
+      assert (y \notin L) as HL by auto.
+      specialize (Hs2 y HL). apply narrow_subtyping with (G':=G & y ~ T) in Hs2.
+      assumption. apply* subenv_last. apply* Hok.
+      apply* HTsub. apply* subenv_last. apply* Hok.
   - pose proof (H (eq_refl _)) as [? Contra]; inversion Contra.
+  - inversion Heq.
 Qed.
