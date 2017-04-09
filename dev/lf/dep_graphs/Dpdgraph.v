5c5e6f0e17869cc1e4c09703b33a45ffcb0bb54c
Ifaz Kabir <ifazk@users.noreply.github.com>
2017-04-09 15:52:27 -0400

    Added Tight_possible_types_val to dep_graph for tight-proof

--- dev/lf/dep_graphs/Dpdgraph.v
+++ dev/lf/dep_graphs/Dpdgraph.v
@@ -5,14 +5,15 @@ Require Coq.Program.Equality.
 
 Require Definitions.
 Require Weakening.
-Require Wellformed_store.
 Require Some_lemmas.
 Require Precise_flow.
 Require Good_types.
-Require Tight_possible_types.
-Require Substitution.
 Require Narrowing.
+Require Tight_possible_types.
 Require General_to_tight.
+Require Tight_possible_types_val.
+Require Wellformed_store.
+Require Substitution.
 Require Canonical_forms1.
 Require Canonical_forms2.
 Require Safety.
@@ -22,14 +23,15 @@ Set DependGraph File "tight_wo_defs.dpd".
 Print FileDependGraph
 (*Definitions*)
 Weakening
-Wellformed_store
 Some_lemmas
 Precise_flow
 Good_types
-Tight_possible_types
-Substitution
 Narrowing
+Tight_possible_types
 General_to_tight
+Tight_possible_types_val
+Wellformed_store
+Substitution
 Canonical_forms1
 Canonical_forms2
 Safety
@@ -40,14 +42,15 @@ Set DependGraph File "tight_wo_defs_slem.dpd".
 Print FileDependGraph
 (*Definitions*)
 Weakening
-Wellformed_store
 (*Some_lemmas*)
 Precise_flow
 Good_types
-Tight_possible_types
-Substitution
 Narrowing
+Tight_possible_types
 General_to_tight
+Tight_possible_types_val
+Wellformed_store
+Substitution
 Canonical_forms1
 Canonical_forms2
 Safety
