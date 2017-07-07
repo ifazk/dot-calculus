7bc72c9e20b9c7c4510fc0369552e7e9c907e5a0
Marianna Rapoport <mrapoport@uwaterloo.ca>
2017-07-07 15:09:36 -0400

    removed wellformed store

--- src/simple-proof/proof/Canonical_forms.v
+++ src/simple-proof/proof/Canonical_forms.v
@@ -13,6 +13,30 @@ Require Import Invertible_typing.
 Require Import General_to_tight.
 Require Import Wellformed_store.
 Require Import Substitution.
+Require Import Weakening.
+
+Lemma corresponding_types: forall G s x T,
+    G ~~ s ->
+    inert G ->
+    binds x T G ->
+    (exists v, binds x v s /\
+          G |- trm_val v : T).
+Proof.
+  introv Hwf Hin BiG. induction Hwf.
+  - false* binds_empty_inv.
+  - destruct (classicT (x = x0)).
+    + subst. apply binds_push_eq_inv in BiG. subst.
+      exists v. repeat split~. apply~ weaken_ty_trm.
+    + apply binds_push_neq_inv in BiG; auto.
+      assert (Hin': inert G).
+      {
+        inversions Hin.
+        - false* empty_push_inv.
+        - destruct (eq_push_inv H2) as [Hx [Hv HG]]. subst*.
+      }
+      specialize (IHHwf Hin' BiG) as [v' [Bis Ht]].
+      exists v'. repeat split~. apply~ weaken_ty_trm.
+Qed.
 
 Lemma invertible_val_to_precise_lambda: forall G v S T,
     G |-##v v : typ_all S T ->
