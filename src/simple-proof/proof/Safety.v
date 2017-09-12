9b870345e8c60c975374b6cf0b9521ade1bdeac5
Marianna Rapoport <mrapoport@uwaterloo.ca>
2017-09-12 15:00:14 -0400

    doc

--- src/simple-proof/proof/Safety.v
+++ src/simple-proof/proof/Safety.v
@@ -35,17 +35,17 @@ Proof.
     * SCase "t = trm_var a".
       destruct (var_typing_implies_avar_f Ht); subst.
       right. exists (open_trm x u). constructor.
-    * SCase "t = trm_val".
+    * SCase "t = trm_val v".
       destruct (val_typing Ht) as [T' [Htp Hs]].
       pick_fresh y. assert (y \notin L) as Hy by auto.
       admit.
-    * SCase "t = trm_sel".
+    * SCase "t = trm_sel a t".
       right. destruct (IHHt Hi e Hwf) as [Hnf | [t' Hr]]. inversion Hnf.
       eexists. constructor*.
-    * SCase "t = trm_app".
+    * SCase "t = trm_app a a0".
       right. destruct (IHHt Hi e Hwf) as [Hnf | [t' Hr]]. inversion Hnf.
       eexists. constructor*.
-    * SCase "t = trm_let".
+    * SCase "t = trm_let t1 t2".
       right. eexists. constructor.
 Qed.
 
